1f0d1e7f0946e64ff78b7d500a9938298dbffd8a
davidAffman <affmandavid@gmail.com>
2024-02-13 23:17:46 +0200

    add jwt token

--- netlify/functions/register/register.ts
+++ netlify/functions/register/register.ts
@@ -1,15 +1,29 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import jwt from "jsonwebtoken";
+
 interface AdminRegisterInput {
-    username:'string';
-    password:'string'
+  username: "string";
+  password: "string";
 }
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    const {body} = event
-    const input:AdminRegisterInput = JSON.parse(body!).input.admin
-    return {
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  const { body } = event;
+  const input: AdminRegisterInput = JSON.parse(body!).input.admin;
+
+  const accessToken = jwt.sign({
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-default-role": "admin",
+      "x-hasura-allowed-roles": ["admin"],
+      "x-hasura-user-id": "123",
+    },
+  },'mygreatjwtsecret');
+
+  return {
     statusCode: 200,
-    body: JSON.stringify({ accessToken:"Hello World!!!" }),
+    body: JSON.stringify({ accessToken: accessToken }),
   };
 };
 
