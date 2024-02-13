3e9a63ccb9f9efed319a3da2666283034bbdbe76
davidAffman <affmandavid@gmail.com>
2024-02-13 23:04:35 +0200

    AdminRegisterInput

--- netlify/functions/register/register.ts
+++ netlify/functions/register/register.ts
@@ -1,7 +1,13 @@
 import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+interface AdminRegisterInput {
+    username:'string';
+    password:'string'
+}
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  return {
+    const {body} = event
+    const input:AdminRegisterInput = JSON.parse(body!).input.admin
+    return {
     statusCode: 200,
     body: JSON.stringify({ accessToken:"Hello World!!!" }),
   };
